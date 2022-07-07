b97237f22e0a5c2242325f56b07de76f0367c816
[FelipeET] <felipe.etchevers@correo.ucu.edu.uy>
2022-07-07 20:05:17 -0300

    Defensa test

--- src/Test/Library.Test/HandlersTest/PlayingHandlerTest.cs
+++ src/Test/Library.Test/HandlersTest/PlayingHandlerTest.cs
@@ -1,68 +1,36 @@
 using NUnit.Framework;
 using PII_Batalla_Naval;
+using System;
 
 namespace Library.Test
 {
     public class PlayingHandlerTest
     { 
     //No pudimos completar este test debido al funcionamiento de nuestro PlayingHandler con bucles.
-          /*
+        public Match match = Match.Instance;
         [Test]
-        public void ShootCommand1Test()
-        { 
-            Player playerT1 = new Player("Pepe");
-            Player playerT2 = new Player("Papa");
-            Match match1 = Match.Instance;
-            match1.CreateGame(playerT1.Name, playerT2.Name);
-            match1.Game.ChangePhase(GamePhase.GameRunning);
-            match1.Game.P1.StatusOnTurn();
-
-
-            string expected = $"Es el turno de {match1.Game.P1.Name} de atacar (use el comando /disparar)";
-            string response= string.Empty;
-            string message="/disparar"; 
-            PlayingHandler A = new PlayingHandler(null);
-            A.Handle(message, out response);
-            Assert.AreEqual(expected, response);
-        } 
-        
-        [Test]
-        public void ShootCommand2Test()
-        { 
-            Player playerT1 = new Player("Pepe");
-            Player playerT2 = new Player("Papa");
-            Match match1 = Match.Instance;
-            match1.CreateGame(playerT1.Name, playerT2.Name);
-            match1.Game.ChangePhase(GamePhase.GameRunning);
-            match1.Game.P2.StatusOnTurn();
-
-
-            string expected = $"Es el turno de {match1.Game.P2.Name} de atacar (use el comando /disparar)";
-            string response= string.Empty;
-            string message="/disparar"; 
-            PlayingHandler A = new PlayingHandler(null);
-            A.Handle(message, out response);
-            Assert.AreEqual(expected, response);
+        public void HitCounterTest()
+        {
+            Player playerT1 = new Player("B");
+            Player playerT2 = new Player("A");
+            match.CreateGame(playerT1.Name, playerT2.Name);
+            match.Game.ChangePhase(GamePhase.GameRunning);
+            match.Game.P1.StatusOnTurn();
+            int expected= 0;
+            int A= (match.Game.P1.PlayerBoard.HitCounter+match.Game.P2.PlayerBoard.HitCounter);
+            Assert.AreEqual(expected, A);
         }
         [Test]
-        public void InvalidShootCommand2Test()
-        { 
-            Player playerT1 = new Player("Pepe");
-            Player playerT2 = new Player("Papa");
-            Match match1 = Match.Instance;
-            match1.CreateGame(playerT1.Name, playerT2.Name);
-            match1.Game.ChangePhase(GamePhase.GameRunning);
-            match1.Game.P2.StatusOnTurn();
-
-
-            string expected = "Comando invlaido, intente nuevamente";
-            string response= string.Empty;
-            string message="/a"; 
-            PlayingHandler A = new PlayingHandler(null);
-            A.Handle(message, out response);
-            Assert.AreEqual(expected, response);
-        } */
-
-
+        public void WatCounterTest()
+        {
+            Player playerT1 = new Player("B");
+            Player playerT2 = new Player("A");
+            match.CreateGame(playerT1.Name, playerT2.Name);
+            match.Game.ChangePhase(GamePhase.GameRunning);
+            match.Game.P1.StatusOnTurn();
+            int expected= 0;
+            int A= (match.Game.P1.PlayerBoard.WatCounter+match.Game.P2.PlayerBoard.WatCounter);
+            Assert.AreEqual(expected, A);
+        }
     }
 }
